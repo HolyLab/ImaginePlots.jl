6888cf35eca89e97fce0f116e83e7ddcb088799b
Cody <codyjgreer@gmail.com>
2017-06-21 15:20:09 -0500

    Catch already-installed packages in build script

--- deps/build.jl
+++ deps/build.jl
@@ -1,2 +1,10 @@
+try
 Pkg.clone("https://github.com/HolyLab/ImagineInterface.git")
+catch
+    print("Could not clone ImagineInterface.  Is it already installed?\n")
+end
+try
 Pkg.clone("https://github.com/ajkeller34/UnitfulPlots.jl.git")
+catch
+    print("Could not clone UnitfulPlots.  Is it already installed?\n")
+end
