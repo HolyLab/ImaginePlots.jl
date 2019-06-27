ad7139fdd3246d99b546ae6ceb1cf79aa0e2676b
Cody-G <codyjgreer@gmail.com>
2019-06-27 17:29:20 -0500

    Update for julia 1.0 / Pkg3, add test, don't force default Plots backend

--- deps/build.jl
+++ deps/build.jl
@@ -1,10 +1,3 @@
-try
-Pkg.clone("https://github.com/HolyLab/ImagineInterface.git")
-catch
-    print("Could not clone ImagineInterface.  Is it already installed?\n")
-end
-try
-Pkg.clone("https://github.com/ajkeller34/UnitfulPlots.jl.git")
-catch
-    print("Could not clone UnitfulPlots.  Is it already installed?\n")
-end
+using Pkg
+Pkg.add(PackageSpec(url="https://github.com/PainterQubits/UnitfulPlots.jl.git"))
+Pkg.resolve()
